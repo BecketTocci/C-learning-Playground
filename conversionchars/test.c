8aa8ed68627e6de43427dcfe8e9336a55d4f08f6
BecketTocci <becket@tocci.org>
2025-07-02 14:10:13 -0500

    Update the first test slightly.

--- conversionchars/test.c
+++ conversionchars/test.c
@@ -4,7 +4,7 @@
 int8_t f;
 
 int main(){
-    printf("What number is the charector?");
+    printf("What number is the charector in ASCII?");
     scanf("%hhi",&f);
     printf("%c\n",f);
     return 0;
